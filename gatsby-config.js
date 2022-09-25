8a4c8ede2187447677121e23121b17c9dc4b93b7
Josh Snyder <joshua.t.snyder@gmail.com>
2022-09-25 15:44:59 -0500

    made some small changes to see how this might work

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,9 +7,9 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
+    siteTitle: `Josh`,
+    siteTitleAlt: `Josh - Gatsby Starter Portfolio`,
+    siteHeadline: `Josh - Gatsby Theme from @lekoarts`,
     siteUrl: `https://cara.lekoarts.de`,
     siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
     siteLanguage: `en`,
